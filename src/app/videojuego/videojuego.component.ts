db158aca20833675806e3a325bc09e29edc6b9f7
Carlos Montes <carlos.montes88@gmail.com>
2023-07-28 18:10:21 -0500

    added the template of the videojuego component in an external file, and added two new attributes to the videojuego component

--- src/app/videojuego/videojuego.component.ts
+++ src/app/videojuego/videojuego.component.ts
@@ -2,19 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'videojuego',
-    template: `
-        <h2>Componente de Videojuegos</h2>
-        <ul>
-            <li>GTA</li>
-            <li>Prince of Persia</li>
-            <li>Tekken</li>
-            <li>Mario</li>
-        </ul>
-        `
+    templateUrl: './videojuego.component.html'
 })
 export class VideojuegoComponent {
+    public titulo: string;
+    public listado: string;
 
     constructor(){
+        this.titulo = "Componente de Videojuegos";
+        this.listado = "Listado de los juegos más populares";
+
         console.log("Se ha cargado el componente: videojuego.component.ts");
     }
 }
