971d149260cda7341023dd416c17b2fc05c2c08b
Carlos Montes <carlos.montes88@gmail.com>
2023-07-29 11:30:06 -0500

    The configuration file (configuration object) was imported to the main component, to initialize its attributes. In your component view (app.component.html) added css properties with [ngStyle] in the div tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Configuracion } from './models/configuracion';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Master de JavaScript y Angular';
+  public descripcion: string = "";
   public mostrar_videojuegos: boolean = true;
+  public config;
+
+  constructor() {
+    this.config = Configuracion;
+    this.title = Configuracion.titulo;
+    this.descripcion = Configuracion.descripcion;
+  }
 
   ocultarVideojuegos(value:boolean){
     this.mostrar_videojuegos = value;
