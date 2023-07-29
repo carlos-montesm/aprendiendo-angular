aabb865fc44e2b2af2cf9d9a4cb9434b2a4e838f
Carlos Montes <carlos.montes88@gmail.com>
2023-07-28 20:27:34 -0500

    added the zapatillas component, and displays a list of brands in app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideojuegoComponent
+    VideojuegoComponent,
+    ZapatillasComponent
   ],
   imports: [
     BrowserModule
