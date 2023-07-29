3989e2fe020c58586c920da07f84d96f6bf0b001
Carlos Montes <carlos.montes88@gmail.com>
2023-07-28 21:52:58 -0500

    Added Lifecycle hooks to the video game component, and added and removed the same component with buttons in the app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Master de JavaScript y Angular';
+  public mostrar_videojuegos: boolean = true;
+
+  ocultarVideojuegos(value:boolean){
+    this.mostrar_videojuegos = value;
+  }
 }
