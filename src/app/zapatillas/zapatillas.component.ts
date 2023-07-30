b63984f11bc2ae1b719f4486cd492cb557803831
Carlos Montes <carlos.montes88@gmail.com>
2023-07-29 20:45:10 -0500

    ngIf directiva was used to display an offer in the zapatilla component's view, using css styling and a conditional (if)

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -12,7 +12,7 @@ export class ZapatillasComponent implements OnInit {
 
     constructor() {
         this.zapatillas = [
-            new Zapatilla('Nike Airmax', 'Nike', 'Rojas', 190, true),
+            new Zapatilla('Nike Airmax', 'Nike', 'Rojas', 40, true),
             new Zapatilla('Reebok Classic', 'Reebook', 'Blanco', 80, true),
             new Zapatilla('Nike Runner MD', 'Nike', 'Negras', 60, true),
             new Zapatilla('Adidas Yezzy', 'Adidas', 'Gris', 180, false)
