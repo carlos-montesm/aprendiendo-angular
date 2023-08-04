6f759a6026bf8b801bab4383add7faf7fea51e21
Carlos Montes <carlos.montes88@gmail.com>
2023-08-03 23:25:26 -0500

    Angular's Pipes were used to display the date in two different formats in the external component's view

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -11,6 +11,7 @@ export class ExternoComponent implements OnInit {
 
   public user: any;
   public userId: any;
+  public fecha: any;
 
   constructor(
     private _peticionesService: PeticionesService
@@ -19,6 +20,7 @@ export class ExternoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.fecha = new Date(2019, 5, 20);
     this.cargaUsuario();
   }
 
